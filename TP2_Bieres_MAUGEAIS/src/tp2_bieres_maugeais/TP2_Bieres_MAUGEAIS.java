323363df704ea0a089233fd5df8471c4efa04944
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-25 15:42:25 +0200

    5 bières crées

---
+++ TP2_Bieres_MAUGEAIS/src/tp2_bieres_maugeais/TP2_Bieres_MAUGEAIS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_maugeais;
+
+/**
+ *
+ * @author thoma
+ */
+public class TP2_Bieres_MAUGEAIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls",7.0,"Dubuisson") ;
+        uneBiere.lireEtiquette();
+        BouteilleBiere deuxièmeBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe");
+        deuxièmeBiere.lireEtiquette();
+        BouteilleBiere troisièmeBiere = new BouteilleBiere("Skoll",6.0,"Brasserie Kronenbourg");
+        troisièmeBiere.lireEtiquette();
+        BouteilleBiere quatrièmeBiere = new BouteilleBiere("Desperados",5.9,"Brasserie Heineken");
+        quatrièmeBiere.lireEtiquette();
+        BouteilleBiere cinquièmeBiere = new BouteilleBiere("Heineken,",5.0,"Brasserie Heineken");
+        cinquièmeBiere.lireEtiquette();
+        
+
+
+    }
+    
+}
