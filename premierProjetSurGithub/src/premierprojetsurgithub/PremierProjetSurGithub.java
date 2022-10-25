9c0c7e99c14c4e8d866e4d6ad35440377e5d4791
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-25 14:39:26 +0200

    Ajout du messgage de bienvenue

--- premierProjetSurGithub/src/premierprojetsurgithub/PremierProjetSurGithub.java
+++ premierProjetSurGithub/src/premierprojetsurgithub/PremierProjetSurGithub.java
@@ -15,6 +15,8 @@ public class PremierProjetSurGithub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("bonjour");
+
     }
     
 }
