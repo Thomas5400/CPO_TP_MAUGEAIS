a88b5c8db29d3956c1002c5a4426e43f3eef4292
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 14:35:38 +0200

    Ajout de Override toString + convertisseurs

--- TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/Convertisseur.java
+++ TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/Convertisseur.java
@@ -10,36 +10,57 @@ package tp2_convertisseurobjet_maugeais;
  */
 public class Convertisseur {
     int nbConversions;
+    double K_C ;
+    double C_K;
+    double C_F;
+    double F_C;
+    double K_F;
+    double F_K;
     
-    public void Convertisseur(){
-        
+    public Convertisseur(){
+        nbConversions = 0;
     }
-    public double CelsiusversKelvin(double) {
-        
-    }
-    
-    public double KelvinversCelsius(double) {
-        
-    }
-    
-    public double FarenheitversCelsius(double) {
-        
-    }
-    
-    public double CelsiusversFarenheit(double) {
-        
+    public double CelsiusversKelvin(double D_Celsius) {
+        C_K = D_Celsius+237.15;
+        nbConversions += 1 ;
+        return C_K;
     }
     
-    public double FarenheitversKelvin(double) {
-        
+    public double KelvinversCelsius(double D_Kelvin) {
+        K_C = D_Kelvin -273.15;
+        nbConversions += 1 ;
+        return  K_C;
     }
     
-    public double KelvinversFarenheit(double) {
-        
+    public double FarenheitversCelsius(double D_Farenheit) {
+        F_C = (D_Farenheit-32) /1.8;
+        nbConversions += 1 ;
+        return F_C;
     }
     
-    public String toString() {
+    public double CelsiusversFarenheit(double D_Celsius) {
+        C_F = D_Celsius*1.8 +32;
+        nbConversions += 1 ;
+        return C_F;
     }
+    
+    public double FarenheitversKelvin(double D_Farenheit) {
+        F_K = (D_Farenheit-32)/1.8 + 273.15;
+        nbConversions += 1 ;
+        return F_K;
+    }
+    
+    public double KelvinversFarenheit(double D_Kelvin) {
+        K_F = (D_Kelvin - 273.15)*1.8 +32;
+        nbConversions += 1 ;
+        return K_F;
+    }
+    
+    @Override
+    public String toString () {
+    return "nb de conversions"+ nbConversions;
+    }
+
     
     
 }
