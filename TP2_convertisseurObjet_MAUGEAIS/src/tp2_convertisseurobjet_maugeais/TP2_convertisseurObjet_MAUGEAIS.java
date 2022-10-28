203417fe2ed35ce9c47dd5f2a560013ca73d94b2
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 15:29:40 +0200

    Ajout de la fonctionalité "Menu" (Questions et réponses aux valeurs de l'utilisateur)

--- TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/TP2_convertisseurObjet_MAUGEAIS.java
+++ TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/TP2_convertisseurObjet_MAUGEAIS.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_maugeais;
 
+import java.util.Scanner;
+
 /**
  *
  * @author thoma
@@ -27,16 +29,42 @@ public class TP2_convertisseurObjet_MAUGEAIS {
         
         System.out.println(Cel.toString());
         System.out.println(Kel.toString());
-
-
         
+        System.out.println("Quelle conversion voulez vous effectuer ? \n 1 pour Celsius vers Autre \n 2 pour Farenheit vers autre\n 3 pour Kelvin vers autre  ");
+        Scanner sc = new Scanner(System.in);
         
-        
-        
-        
-        
-        
-        
+        int a = sc.nextInt();
+        if (a == 1){
+            System.out.println("choisissez la valeur a convertir :");
+            Scanner T = new Scanner(System.in);
+            double c = T.nextDouble();
+            System.out.println("La valeur en Kelvin est : "+ Cel.CelsiusversKelvin(c));
+            System.out.println("La valeur en Farenheit est : "+ Cel.CelsiusversFarenheit(c));
+            
+        }
+        else {
+            if (a == 2){
+                System.out.println("choisissez la valeur a convertir :");
+                Scanner U = new Scanner(System.in);
+                double d = U.nextDouble();
+                System.out.println("La valeur en Kelvin est : "+ Cel.FarenheitversKelvin(d));
+                System.out.println("La valeur en Celsius est : "+ Cel.FarenheitversCelsius(d));
+            }
+            else{
+                if (a==3){
+                    System.out.println("choisissez la valeur a convertir :");
+                    Scanner V = new Scanner(System.in);
+                    double e = V.nextDouble();
+                    System.out.println("La valeur en Kelvin est : "+ Cel.KelvinversFarenheit(e));
+                    System.out.println("La valeur en Celsius est : "+ Cel.KelvinversCelsius(e));
+                }
+                else{
+                    System.out.println("ERREUR Veuillez relancer le programme");
+                }
+            }
+                  
+        }
+             
     }
     
 }
