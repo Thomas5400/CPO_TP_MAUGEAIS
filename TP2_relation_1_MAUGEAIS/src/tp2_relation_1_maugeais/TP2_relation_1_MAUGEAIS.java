a39136124ee520dc620a8dd97220bbca92581090
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 17:27:17 +0200

    Commentaires supp pour explications

--- TP2_relation_1_MAUGEAIS/src/tp2_relation_1_maugeais/TP2_relation_1_MAUGEAIS.java
+++ TP2_relation_1_MAUGEAIS/src/tp2_relation_1_maugeais/TP2_relation_1_MAUGEAIS.java
@@ -16,7 +16,7 @@ public class TP2_relation_1_MAUGEAIS {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Voiture uneClio = new Voiture ("Clio", "Renault",5 ) ;
+        Voiture uneClio = new Voiture ("Clio", "Renault",5 ) ;  // Initialisation avec le constructeur des voitures et propriétaires
         Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
@@ -25,15 +25,25 @@ public class TP2_relation_1_MAUGEAIS {
         System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
 // Le code fonctionne bien
         
-    bob.liste_voitures[0] = uneClio ;
-    bob.nbVoitures = 1 ;
+    bob.liste_voitures[0] = uneClio ;// 1ère voiture de Bob
+    bob.nbVoitures += 1 ;
     uneClio.Proprietaire = bob ;
     
-    System.out.println("la premiere voiture de Bob est   " +bob.liste_voitures[0] ) ;
+    bob.liste_voitures[1] = une2008 ; // Deuxième voiture pour Bob
+    bob.nbVoitures += 1 ;
+    une2008.Proprietaire = bob ;
+    System.out.println("\nla premiere voiture de Bob est   " +bob.liste_voitures[0] ) ;
+    System.out.println("la deuxieme voiture de Bob est   " +bob.liste_voitures[1] ) ;
 
-        
-        
-        
+    reno.liste_voitures[0] = uneMicra ;
+    reno.nbVoitures +=1 ;
+    uneMicra.Proprietaire = reno ;
+    
+    reno.liste_voitures[1] = uneAutreClio ; // ajout de 2 voitures à Reno
+    reno.nbVoitures +=1 ;
+    uneAutreClio.Proprietaire = reno ;
+    System.out.println("\nla premiere voiture de Reno est   " +bob.liste_voitures[0] ) ;
+    System.out.println("la deuxieme voiture de Reno est   " +bob.liste_voitures[1] ) ;   
     }
     
 }
