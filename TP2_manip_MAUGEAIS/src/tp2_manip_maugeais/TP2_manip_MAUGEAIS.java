38b357399d9fa1286570d5e105af1b46fb537d31
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 15:56:15 +0200

    Operations sur les assiettes

--- TP2_manip_MAUGEAIS/src/tp2_manip_maugeais/TP2_manip_MAUGEAIS.java
+++ TP2_manip_MAUGEAIS/src/tp2_manip_maugeais/TP2_manip_MAUGEAIS.java
@@ -15,6 +15,31 @@ public class TP2_manip_MAUGEAIS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Tartiflette assiette1 = new Tartiflette(500) ;// initialisation des assietes
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ; 
+        
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+//affichage des calories de assiette 3 et 2.
+
+
+        assiette2.nbCalories += 150;
+        
+        System.out.println("\n Ajout de 150 calories à assiette 2 \n \n") ;
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+// Les calories changent bien pour assiette 3 aussi.
+
+
+        int v= assiette1.nbCalories; // inversion de assiette 1 et 2 
+        assiette1.nbCalories = assiette2.nbCalories;
+        assiette2.nbCalories = v;
+        
+        System.out.println("\n Inversion de assiettes 1 et 2\n \n") ;
+        System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
     }
     
 }
