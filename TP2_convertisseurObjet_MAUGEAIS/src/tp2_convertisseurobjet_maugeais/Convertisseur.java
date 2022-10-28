f9de1c5fe77d8599bf0b57bba76491eb59f760e5
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 18:47:07 +0200

    Ajouts des commentaire au TP convertisseur

--- TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/Convertisseur.java
+++ TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/Convertisseur.java
@@ -8,8 +8,8 @@ package tp2_convertisseurobjet_maugeais;
  *
  * @author thoma
  */
-public class Convertisseur {
-    int nbConversions;
+public class Convertisseur {//nouvelle classe
+    int nbConversions;// On rentre les différents attributs, un pour chaque conversion à effectuer
     double K_C ;
     double C_K;
     double C_F;
@@ -17,15 +17,15 @@ public class Convertisseur {
     double K_F;
     double F_K;
     
-    public Convertisseur(){
+    public Convertisseur(){//constructeur pour initialiser le nombre de conversion à 0
         nbConversions = 0;
     }
     public double CelsiusversKelvin(double D_Celsius) {
-        C_K = D_Celsius+237.15;
-        nbConversions += 1 ;
-        return C_K;
+        C_K = D_Celsius+237.15;//calcul de la conversion
+        nbConversions += 1 ;//on rajoute +1 au nombre de conversion . Cette action se repetera pour toutes les conversions
+        return C_K;// on retourne la valeur finale qui est la température convertie dans la nouvelle unité voulue
     }
-    
+    // on répète cela pour toutes les conversion que l'on souhaite effectuer
     public double KelvinversCelsius(double D_Kelvin) {
         K_C = D_Kelvin -273.15;
         nbConversions += 1 ;
@@ -56,9 +56,9 @@ public class Convertisseur {
         return K_F;
     }
     
-    @Override
+    @Override// Cela permet de bien afficher les résultats des conversions
     public String toString() {
-    return "nb de conversions  : "+ nbConversions;
+    return "nb de conversions  : "+ nbConversions;// on affiche de la sorte a avoir du texte puis la valeur calculée pour le nombre de conversion effectue par un objet 
     }
 
     
--- TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/TP2_convertisseurObjet_MAUGEAIS.java
+++ TP2_convertisseurObjet_MAUGEAIS/src/tp2_convertisseurobjet_maugeais/TP2_convertisseurObjet_MAUGEAIS.java
@@ -17,33 +17,33 @@ public class TP2_convertisseurObjet_MAUGEAIS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Convertisseur Cel = new Convertisseur ();
-        System.out.println(Cel.CelsiusversKelvin(33));
+        Convertisseur Cel = new Convertisseur ();//Nouvel objet convertisseur à la base crée juste pour convertir de celsius vers autre mais il peut en réalité tout faire car c'est leXXX de cel.XXX qui fait une conversion ou une autre
+        System.out.println(Cel.CelsiusversKelvin(33));// affichage des résultats des conversion pour des valeur de 33 degres celsius
         System.out.println(Cel.CelsiusversFarenheit(33));
         
-        Convertisseur Kel = new Convertisseur();
+        Convertisseur Kel = new Convertisseur();                    //// De meme , nouvel objet pour degre Kelvin vers autre
         System.out.println(Kel.KelvinversCelsius(300));
         System.out.println(Kel.KelvinversFarenheit(300));
         System.out.println(Kel.KelvinversFarenheit(450));
         
         
-        System.out.println(Cel.toString());
-        System.out.println(Kel.toString());
+        System.out.println(Cel.toString());//affiche le nombre de conversion de l'objet Cel
+        System.out.println(Kel.toString());//affiche le nombre de conversion de l'objet Kel
         
-        System.out.println("Quelle conversion voulez vous effectuer ? \n 1 pour Celsius vers Autre \n 2 pour Farenheit vers autre\n 3 pour Kelvin vers autre  ");
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Quelle conversion voulez vous effectuer ? \n 1 pour Celsius vers Autre \n 2 pour Farenheit vers autre\n 3 pour Kelvin vers autre  ");//Choix de l'utilisateur sur la conversion à effectuer
+        Scanner sc = new Scanner(System.in);//Intègre la réponse donnée par l'utilisateur
         
         int a = sc.nextInt();
-        if (a == 1){
-            System.out.println("choisissez la valeur a convertir :");
-            Scanner T = new Scanner(System.in);
-            double c = T.nextDouble();
-            System.out.println("La valeur en Kelvin est : "+ Cel.CelsiusversKelvin(c));
+        if (a == 1){ // si la reponse est 1 alors 
+            System.out.println("choisissez la valeur a convertir :");//on demande la, valeur à convertir 
+            Scanner T = new Scanner(System.in);//valeur rentrée
+            double c = T.nextDouble();//c devient la valeur rentrée
+            System.out.println("La valeur en Kelvin est : "+ Cel.CelsiusversKelvin(c));// on calcule et affiche proprement les conversions
             System.out.println("La valeur en Farenheit est : "+ Cel.CelsiusversFarenheit(c));
             
         }
         else {
-            if (a == 2){
+            if (a == 2){//Raisonnement similaire pour un autre type de conversion, revoir la partie "si a==1"
                 System.out.println("choisissez la valeur a convertir :");
                 Scanner U = new Scanner(System.in);
                 double d = U.nextDouble();
@@ -51,7 +51,7 @@ public class TP2_convertisseurObjet_MAUGEAIS {
                 System.out.println("La valeur en Celsius est : "+ Cel.FarenheitversCelsius(d));
             }
             else{
-                if (a==3){
+                if (a==3){//Raisonnement similaire pour un autre type de conversion, revoir la partie "si a==1"
                     System.out.println("choisissez la valeur a convertir :");
                     Scanner V = new Scanner(System.in);
                     double e = V.nextDouble();
@@ -59,7 +59,7 @@ public class TP2_convertisseurObjet_MAUGEAIS {
                     System.out.println("La valeur en Celsius est : "+ Cel.KelvinversCelsius(e));
                 }
                 else{
-                    System.out.println("ERREUR Veuillez relancer le programme");
+                    System.out.println("ERREUR Veuillez relancer le programme");// Si l'utilisateur rentre une autre valeur que 1, 2 ou 3 alors il y a une erreur et un message prévenant l'utilisateur de recommencer
                 }
             }
                   
