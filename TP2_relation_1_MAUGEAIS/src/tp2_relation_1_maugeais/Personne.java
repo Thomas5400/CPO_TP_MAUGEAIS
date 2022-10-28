5982010a0bb77f5bcf072004548179cdbc52acf6
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 18:03:11 +0200

    Fin de la méthode d'automatisation. Avant test

--- TP2_relation_1_MAUGEAIS/src/tp2_relation_1_maugeais/Personne.java
+++ TP2_relation_1_MAUGEAIS/src/tp2_relation_1_maugeais/Personne.java
@@ -23,6 +23,25 @@ public class Personne {
         
     }
     
+    public boolean ajouter_voiture(Voiture voiture_a_ajouter){
+        if (voiture_a_ajouter.Proprietaire == null){
+            return false;
+        }
+        else{
+            if (nbVoitures >= 3){
+                return false;
+            }
+            else{
+                liste_voitures[nbVoitures]=voiture_a_ajouter;
+                nbVoitures+=1;
+                voiture_a_ajouter.Proprietaire = this ;
+                return true;
+            }
+        }
+        
+    }
+            
+            
     @Override
     public String toString () {
     return "Prenom : "+ prenom+ "     Nom : "+ nom;
