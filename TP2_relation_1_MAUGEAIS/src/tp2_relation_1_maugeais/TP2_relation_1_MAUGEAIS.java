8e0334e45dc7eee592e788b9a780d84948fdf3c7
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-10-28 16:27:06 +0200

    Creation projet et classes

---
+++ TP2_relation_1_MAUGEAIS/src/tp2_relation_1_maugeais/TP2_relation_1_MAUGEAIS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_relation_1_maugeais;
+
+/**
+ *
+ * @author thoma
+ */
+public class TP2_relation_1_MAUGEAIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        
+        
+    }
+    
+}
